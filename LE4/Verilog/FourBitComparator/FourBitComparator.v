1622cb46090b05aec40e23868aade89b2c5ed248
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-10-02 20:23:53 +0800

    Four Bit Comparator Project File | Verilog

--- LE4/Verilog/FourBitComparator/FourBitComparator.v
+++ LE4/Verilog/FourBitComparator/FourBitComparator.v
@@ -10,11 +10,9 @@ module FourBitComparator(
 	input 	[3:0]B,
 	output 	[2:0]R
 );
-	wire 		[2:0]common;
 	// A > B @ R2 |R [2] = G (1 if A > B, else 0)
 	assign R[2] = (A > B)? 1'b1 : 1'b0;
-	
-	
+
 	// A = B @ R[1] | R[1] = E (1 if A = B, else 0)
 	assign R[1] = (A == B)? 1'b1 : 1'b0;
 	
